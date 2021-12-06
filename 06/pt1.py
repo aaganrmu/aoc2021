648b3863dcac087c57d4793707be433af6fc45c2
Elmar Wiedemeijer <elmar.wiedemeijer@new10.com>
2021-12-06 11:03:33 +0100

    added some documentation

--- 06/pt1.py
+++ 06/pt1.py
@@ -11,8 +11,13 @@ for fish in fishes:
     school[int(fish)] += 1
 
 def advance_day(school):
+    # element 0 of list are all fish that are due to pop out a baby
+    # pop(0) also shifts all other fish one place left, i.e. decrease their timers
     growth = school.pop(0)
+    # all those ex-preggers fish have their timer reset to 6
     school[6] += growth
+    # all baby fish have their their timer set to 8.
+    # due to pop(0) there is no element 8 anymore so we append it.
     school.append(growth)
     return school
 
