2ee058224c7d15bf8ac3c9739f2c502664f79c92
Elmar Wiedemeijer <elmar.wiedemeijer@new10.com>
2021-12-06 14:10:00 +0100

    some newlines nicer in day 06

--- 06/pt2.py
+++ 06/pt2.py
@@ -10,6 +10,7 @@ school = [0] * 9
 for fish in fishes:
     school[int(fish)] += 1
 
+
 def advance_day(school):
     # element 0 of list are all fish that are due to pop out a baby
     # pop(0) also shifts all other fish one place left, i.e. decrease their timers
@@ -21,10 +22,9 @@ def advance_day(school):
     school.append(growth)
     return school
 
-for day in range(0,256):
+
+for day in range(0, 256):
     advance_day(school)
 
 
 print(sum(school))
-
-
